0f481a4216ae4ab9020cbe4312375c6a4511b18a
SW <snowwolf9x@gmail.com>
2017-11-06 02:09:31 +0700

    Add task

--- src/App.js
+++ src/App.js
@@ -35,27 +35,14 @@ class App extends Component {
         })
     }
 
-    onGenerateData = () => {
-        var tasks = [
-            {
-                id: this.generateID(),
-                name : 'Learn',
-                status: true
-            },
-            {
-                id: this.generateID(),
-                name : 'Game',
-                status: false
-            },
-            {
-                id: this.generateID(),
-                name : 'PingPong',
-                status: true
-            },
-        ];
-        // console.log('before : '+ typeof(tasks[0].status));
+    onSubmit = (data) => {
+        var tasks = this.state.tasks;
+        data.id = this.generateID();
+        tasks.push(data);
+        this.setState({
+            tasks : tasks
+        })
         localStorage.setItem('tasks', JSON.stringify(tasks));
-        // console.log('middle : '+ typeof(JSON.parse(localStorage.getItem('tasks'))[0].status));
     }
 
     s4() {
@@ -68,18 +55,16 @@ class App extends Component {
 
   render() {
     var {tasks, isDisplayForm} = this.state;
-    var elmTaskFrom = isDisplayForm ? <AddTaskForm onCloseForm = {this.onCloseForm}/> : '';
+    var elmTaskFrom = isDisplayForm 
+        ? <AddTaskForm onSubmit={this.onSubmit} onCloseForm = {this.onCloseForm}/>
+        : '';
     return (
       <div className="container">
             <div className="text-center">
                 Task Management
                 <hr></hr>
             </div>
-
             {elmTaskFrom}
-
-            
-
             <div className={isDisplayForm ? "col-xs-8 col-sm-8 col-md-8 col-lg-8" : "col-xs-12 col-sm-12 col-md-12 col-lg-12"}>
                 <div className="row">
                     <button 
@@ -90,16 +75,7 @@ class App extends Component {
                         <i className="fa fa-plus" aria-hidden="true"></i>
                         Add Task
                     </button>
-                    <button 
-                        type="button" 
-                        className="btn btn-danger ml-15"
-                        onClick={this.onGenerateData}
-                    >
-                    <i className="fa fa-plus" aria-hidden="true"></i>
-                    Generate Data
-                    </button>
                 </div>
-                
                 <TaskControl />
                 <TaskList tasks = { tasks }/>
             </div>  
--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -2,8 +2,42 @@ import React, { Component } from 'react';
 
 class AddTaskForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            name : '',
+            status: false
+        }
+    }
+
     onCloseForm = () => {
         this.props.onCloseForm();
+    }
+
+    onChange = (event) => {
+        var target = event.target;
+        var name = target.name;
+        var value = target.value;
+        if (name === 'status') {
+            value = target.value === 'true' ? true : false;
+        }
+        this.setState({
+            [name] : value
+        });
+    }
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state);
+        this.onClear();
+        this.onCloseForm();
+    }
+
+    onClear = () => {
+        this.setState({
+            name : '',
+            status : false
+        })
     }
 
   render() {
@@ -21,18 +55,46 @@ class AddTaskForm extends Component {
                         </h3>
                     </div>
                     <div className="panel-body">
-                        <form action="" method="POST">
+                        <form onSubmit = {this.onSubmit}>
                             <div className="form-group">
                                 <label >Name : </label>
-                                <input type="text" className="form-control" id="" placeholder="Input field" />
+                                <input 
+                                    type="text" 
+                                    name="name"
+                                    className="form-control" 
+                                    placeholder="Input field" 
+                                    value = {this.state.name}
+                                    onChange = {this.onChange}
+                                />
                                 <label >Status : </label>
-                                <select name="" id="input" className="form-control">
-                                    <option value=""> Active </option>
-                                    <option value=""> Deactive </option>
+                                <select 
+                                    name="status" 
+                                    id="input" 
+                                    className="form-control"
+                                    value = {this.state.status}
+                                    onChange = {this.onChange}
+                                >
+                                    <option value={true}> Active </option>
+                                    <option value={false}> Deactive </option>
                                 </select>
                             </div>
-                            <button type="submit" className="btn btn-success mr-5"><i className="fa fa-plus" aria-hidden="true"></i> Save</button>
-                            <button type="submit" className="btn btn-danger"><i className="fa fa-ban" aria-hidden="true"></i> Cancel</button>
+                            <button 
+                                type="submit" 
+                                className="btn btn-success mr-5"
+                            >
+                                <i className="fa fa-plus" aria-hidden="true">
+                                </i>
+                                Save
+                            </button>
+                            <button 
+                                type="reset" 
+                                className="btn btn-danger"
+                                onClick = {this.onClear}
+                            >
+                                <i className="fa fa-ban" aria-hidden="true">
+                                </i> 
+                                Cancel
+                            </button>
                         </form>
                     </div>
                 </div>
